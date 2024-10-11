83268affd59e5632b850a5318991fac96a3893f2
yaxin-liu24 <yaxin.liu24@imperial.ac.uk>
2024-10-12 00:41:07 +0100

    Delete week2 folder

--- week2/code/basic_io1.py
+++
@@ -1,8 +0,0 @@
-f= open("/home/yaxin-liu/documents/CMEECoursework/week2/sandbox/test.txt","r")
-# for line in f:
-#     print(line)
-for line in f:
-    if len(line.strip())>0:
-        print(line)
-
-f.close
--- week2/code/basic_io2.py
+++
@@ -1,6 +0,0 @@
-list_save= range(100)
-f= open|("/home/yaxin-liu/documents/CMEECoursework/week2/sandbox/test.txt","w")
-for i in list_save:
-    f.write(str(i)+"\n")
-f.close
-
