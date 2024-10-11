da93037cfe06e0bf29633db514184612364de4e6
yaxin-liu24 <yaxin.liu24@imperial.ac.uk>
2024-10-12 00:24:47 +0100

    Full first commit of new project

---
+++ week2/code/basic_io2.py
@@ -0,0 +1,6 @@
+list_save= range(100)
+f= open|("/home/yaxin-liu/documents/CMEECoursework/week2/sandbox/test.txt","w")
+for i in list_save:
+    f.write(str(i)+"\n")
+f.close
+
---
+++ week2/sandbox/classexercise.py
@@ -0,0 +1,12 @@
+x = 6
+for i in range(x):
+        if i > 3 :
+                print (i)
+# range()
+for i in range(2,20,2):
+        print(i)
+# iterater&next
+myiterater=[1,2,3,4]
+myit=iter(myiterater)
+next(myit) 
+#
